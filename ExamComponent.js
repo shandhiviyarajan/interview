66ebce323cef597b364810439147192641e3e459
Shan Dhiviyarajan <prashasoft@gmail.com>
2023-05-18 10:36:46 +0530

    exam component

---
+++ ExamComponent.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import {Button, Text, TextInput, View} from 'react-native';
+const ExamComponent = () => {
+  const [inputValue, setInputValue] = React.useState('');
+  const [oddNumber, setOddNumber] = React.useState(false);
+
+  const acceptNumber = value => {
+    if (value && isNaN(value)) {
+      return;
+    }
+    setInputValue(value);
+  };
+  const checkOddNumber = () => {
+    let number_type =
+      inputValue % 2 === 0 ? 'This is a even number' : 'This is a odd number';
+    setOddNumber(number_type);
+  };
+
+  return (
+    <View>
+      <Text>ExamComponent</Text>
+      <TextInput
+        type="number"
+        placeholder="Enter Number"
+        onChangeText={value => acceptNumber(value)}
+      />
+      <Button title="Check Number" onPress={checkOddNumber} />
+
+      {oddNumber && inputValue && (
+        <View>
+          <Text>{oddNumber}</Text>
+        </View>
+      )}
+    </View>
+  );
+};
+
+export default ExamComponent;
